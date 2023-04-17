2df187b7aaaf3e95cb9c2bee8af7a3419d673cb8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-17 20:22:02 +0200

    Basic fundamentals

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+// Values and Variables
+let country = "Spain";
+let continent = "Europe";
+let population = 49;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+const isIsland = false;
+let languaje;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof languaje);
+
+// let, const and var
+languaje = "Spanish";
+// isIsland = false; Error
+
+// Basic Operators
+console.log(population / 2);
+console.log(++population);
+console.log(population > 6);
+console.log(population < 33);
+let description = `${country} is in ${continent}, and its ${population} million people speak ${languaje}`;
+console.log(description);
+
+// Strings and Template Literals
+// Already done it
