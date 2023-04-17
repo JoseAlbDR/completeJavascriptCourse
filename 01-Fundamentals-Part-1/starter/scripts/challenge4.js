bc3d538280ccf893127d8b8c3c6dcffb38cb4609
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-18 00:58:48 +0200

    let tip to const

--- 01-Fundamentals-Part-1/starter/scripts/challenge4.js
+++ 01-Fundamentals-Part-1/starter/scripts/challenge4.js
@@ -16,7 +16,7 @@ GOOD LUCK 😀
 */
 
 const bill = 430;
-let tip = bill >= 50 && bill <= 300 ? 15 : 20;
+const tip = bill >= 50 && bill <= 300 ? 15 : 20;
 console.log(
   `The bill was ${bill}, the tip was ${
     (bill * tip) / 100
