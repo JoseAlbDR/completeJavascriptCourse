36644d88a872acc3e67e312b150e2c63c7bce315
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-18 01:46:16 +0200

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,20 @@
 "use strict";
-"use strict";
-"use strict";
+
+let hadDriversLicense = false;
+const passTest = true;
+
+passTest ? (hadDriversLicense = true) : (hadDriversLicense = false);
+console.log(passTest);
+
+// Functions
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+
+const firstCountry = describeCountry("Spain", 49, "Madrid");
+const secondCountry = describeCountry("England", 29, "London");
+const thirdCountry = describeCountry("France", 47, "Paris");
+
+console.log(firstCountry);
+console.log(secondCountry);
+console.log(thirdCountry);
