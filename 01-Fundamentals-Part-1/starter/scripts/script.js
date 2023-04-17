99d420735bbe721641a0ddead062a9997879cd28
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-18 00:28:15 +0200

    Switch statement

--- 01-Fundamentals-Part-1/starter/scripts/script.js
+++ 01-Fundamentals-Part-1/starter/scripts/script.js
@@ -54,3 +54,25 @@ console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
 // } else {
 //   console.log("No borders");
 // }
+
+// The switch Statement
+
+switch (languaje.toLowerCase()) {
+  case "chinese" || "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken languaje");
+    break;
+  default:
+    console.log("Great languaje too :D");
+}
