8ba0e9e7cbc55e4a511088cd15551938314b8fa2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-19 13:05:04 +0200

    Code refactor

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -10,5 +10,32 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // console.log(modalButton.textContent);
 
-// Loop the buttons
-for (let button in btnShowModal) console.log(btnShowModal[button].textContent);
+// // Loop the buttons
+// for (let button in btnShowModal)
+//   btnShowModal[button].addEventListener('click', () => {
+//     console.log(`Button ${btnShowModal[button].textContent} clicked.`);
+//   });
+
+// To close the modal
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// To show the modal
+const showModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', () => showModal());
+
+// X (Close modal button)
+btnCloseModal.addEventListener('click', () => closeModal());
+
+// Esc keypress listener
+modal.addEventListener('keypress', event => {
+  if (event.key === 'Escape') closeModal();
+});
