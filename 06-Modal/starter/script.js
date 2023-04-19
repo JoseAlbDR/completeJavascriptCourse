19c181420c25f2bc552e65046f6f7966d64d76bb
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-19 12:35:27 +0200

    QuerySelectorAll

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Variables querySelector
+const modal = document.querySelector('.modal');
+// querySelector for only one button
+// const btnShowModal = document.querySelector('.show-modal');
+// querySelectorAll for all buttons
+const btnShowModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// console.log(modalButton.textContent);
+
+// Loop the buttons
+for (let button in btnShowModal) console.log(btnShowModal[button].textContent);
