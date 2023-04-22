848ea1a374f5bb519094cf11d8d3ffb4ee890e62
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-23 00:23:50 +0200

    Calling functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -23,3 +23,34 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 createBooking('LH123', 2);
 createBooking('LH123', undefined, 500);
+
+// Calling functions
+const flight = 'LH234';
+const alberto = {
+  name: 'Alberto Delgado',
+  passport: 123456789,
+};
+
+// passenger === alberto
+const checkIn = function (flightNum, passenger) {
+  // Not a good practice
+  (flightNum = 'LH999'), (passenger.name = 'Mr.' + passenger.name);
+
+  if (passenger.passport === 123456789) {
+    alert('Check in.');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, alberto);
+console.log(flight);
+console.log(alberto);
+
+// Example
+const newPassword = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassword(alberto);
+// checkIn(flight, alberto);
