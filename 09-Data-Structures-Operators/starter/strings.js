4cd113a44bfe1c47339cd012a5ec8d53796e9261
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-22 23:56:30 +0200

    Section 9 done

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -111,3 +111,17 @@ button.addEventListener('click', () => {
     }
   });
 });
+
+const flights1 =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const splitFlights = flights1.replaceAll('_', ' ').trim().split('+');
+
+for (const f of splitFlights) {
+  const [departure, origin, destiny, hour] = [...f.split(';')];
+  console.log(
+    `${departure} from ${origin.toUpperCase().slice(0, 3)} to ${destiny
+      .toUpperCase()
+      .slice(0, 3)} (${hour.replace(':', 'h')})`.padStart(41)
+  );
+}
