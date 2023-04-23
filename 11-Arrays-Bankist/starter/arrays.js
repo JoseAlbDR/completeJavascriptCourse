3c103499ccac02deeaaba9b8f77b78b0086ac987
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-23 19:57:16 +0200

    Arrays methods, foreach and so

---
+++ 11-Arrays-Bankist/starter/arrays.js
@@ -0,0 +1,60 @@
+'use strict';
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+/////////////////////////////////////////////////
+// SLICE
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2, 3));
+// Start at index 1 and extract everything except the 2 last
+console.log(arr.slice(1, -2));
+
+// SPLICE Mutate
+// console.log(arr.splice(2));
+// arr.splice(-1);
+// arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE Mutate
+const arr2 = ['j', 'i', 'h', 'g', 'j'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+// const letters = [...arr, ...arr2];
+console.log(letters);
+
+// JOIN
+console.log(letters.join(', '));
+
+// AT
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+
+console.log(arr3.at(-1));
+
+console.log('alberto'.at(-1));
+
+// FOREACH
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+movements.forEach(move =>
+  move > 0
+    ? console.log(`You deposited ${move}`)
+    : console.log(`You withdrew ${Math.abs(move)}`)
+);
+
+movements.forEach((move, index, array) => {
+  console.log(`Movements ${array.join(' |')}`),
+    move > 0
+      ? console.log(`Movement ${index}: you deposited ${move}`)
+      : console.log(`Movement ${index}: you withdrew ${Math.abs(move)}`);
+});
