7364e1ff9e8e98c4cacd2e6166b36e16af879d0b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-28 16:33:33 +0200

    Cronsctuctor

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+class Person {
+  constructor(firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never to this (function inside of constructor function)
+    // this.calcAge = function () {
+    //   console.log(2023 - this.birthYear);
+    // };
+  }
+}
+
+const alberto = new Person('Alberto', 1984);
+console.log(alberto);
+
+// 1. New {} is created
+// 2. function is called, this => {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilde = new Person('Matilde', 1984);
+const leo = new Person('Leonidas', 2020);
+console.log(matilde, leo);
+console.log(alberto instanceof Person);
+
+// Prototipes
