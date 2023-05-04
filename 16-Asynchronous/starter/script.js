d1314d61713b7c1359f52dea6e653b4d046fcb0d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-04 13:12:45 +0200

    Old way AJAX API call

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,38 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// Old way
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(2)} million</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.spa}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('afterbegin', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('spain');
+getCountryData('portugal');
+getCountryData('andorra');
