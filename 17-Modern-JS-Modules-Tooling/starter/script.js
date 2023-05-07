73c4cb766cfbd912972b9d43996890132a061dd2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-07 18:20:36 +0200

    Section 17 done

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -60,3 +60,25 @@ import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart2.addToCart('apple', 4);
 // ShoppingCart2.addToCart('pizza', 2);
 // console.log(ShoppingCart2);
+
+// import cloneDeep from '../../node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from 'lodash-es';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { prodduct: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
